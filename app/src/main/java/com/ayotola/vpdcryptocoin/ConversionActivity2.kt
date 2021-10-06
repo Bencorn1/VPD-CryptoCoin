7d8bb72f8c78416d2ff2cb51dfd3f669012be82b
Bencorn1 <moronkejiayotola@gmail.com>
2021-10-06 22:09:16 +0100

    Laying of the UI activity for the view for the conversion of coins.

---
+++ app/src/main/java/com/ayotola/vpdcryptocoin/ConversionActivity2.kt
@@ -0,0 +1,11 @@
+package com.ayotola.vpdcryptocoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConversionActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversion)
+    }
+}
