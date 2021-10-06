d2efd2a393eaeb063beecf0557e4d16a5ce9f83d
Bencorn1 <moronkejiayotola@gmail.com>
2021-10-07 00:11:06 +0100

    Updates on the laying of the UI activity for the view for coin display.

--- app/src/main/java/com/ayotola/vpdcryptocoin/MainActivity.kt
+++ app/src/main/java/com/ayotola/vpdcryptocoin/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ayotola.vpdcryptocoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.ayotola.vpdcryptocoin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listView = binding.coinListView
+
+//        val coinList = Coin.getRecipesFromFile("recipes.json", this)
+//        val listItems = arrayOfNulls<String>(coinList.size)
+//        for (i in 0 until coinList.size) {
+//            val coin = coinList[i]
+//            listItems[i] = recipe.title
+//        }
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+//        listView.adapter = adapter
+
     }
 }
