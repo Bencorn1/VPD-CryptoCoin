7b12109e9b48b14790f2e701280a42c54006583f
Bencorn1 <moronkejiayotola@gmail.com>
2021-10-07 02:23:26 +0100

    Updates on the UI settings.

--- app/src/main/java/com/ayotola/vpdcryptocoin/ConversionActivity.kt
+++ app/src/main/java/com/ayotola/vpdcryptocoin/ConversionActivity.kt
@@ -2,10 +2,17 @@ package com.ayotola.vpdcryptocoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ayotola.vpdcryptocoin.databinding.ActivityConversionBinding
+import com.ayotola.vpdcryptocoin.databinding.ActivityMainBinding
 
 class ConversionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConversionBinding
+    var coins = arrayOf<String>("BTC", "ETH", "XRP", "Dogecoin", "Dash", "Litecoin")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_conversion)
+        binding = ActivityConversionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
