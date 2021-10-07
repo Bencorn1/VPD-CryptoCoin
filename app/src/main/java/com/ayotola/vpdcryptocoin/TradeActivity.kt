ecf8643a6a533d4c4339c3d38ec05f69f8ab5b24
Bencorn1 <moronkejiayotola@gmail.com>
2021-10-07 02:39:25 +0100

    Updates on the UI settings for the spinner.

--- app/src/main/java/com/ayotola/vpdcryptocoin/TradeActivity.kt
+++ app/src/main/java/com/ayotola/vpdcryptocoin/TradeActivity.kt
@@ -1,11 +1,40 @@
 package com.ayotola.vpdcryptocoin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ayotola.vpdcryptocoin.databinding.ActivityTradeBinding
 
-class TradeActivity : AppCompatActivity() {
+class TradeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityTradeBinding
+    var coins = arrayOf<String>("BTC", "ETH", "XRP", "Dogecoin", "Dash", "Litecoin")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trade)
+        binding = ActivityTradeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spin = binding.spinner
+
+        spin.onItemSelectedListener = this
+
+        val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(this, R.layout.simple_spinner_item, coins)
+
+        ad.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+
+        spin.adapter = ad
     }
+
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+
+        Toast.makeText(applicationContext, coins[position], Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
